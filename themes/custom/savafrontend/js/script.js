f785cd1446a0ae00edeed6a3f8e8c688b75f80d3
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-27 18:02:58 +0200

    add initial custom theme

---
+++ themes/custom/savafrontend/js/script.js
@@ -0,0 +1,7 @@
+(function ($, Drupal) {
+  Drupal.behaviors.savafrontendBehavior = {
+    attach: function (context, settings) {
+      console.log("Savafrontend theme is active!");
+    },
+  };
+})(jQuery, Drupal);
